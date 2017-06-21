772404860ecf67d637530466d90ccc220493e396
Wilmer Vazquez <vazquezwilmer@gmail.com>
2017-06-21 00:49:09 -0400

    Finalize template
    Adds ejs modularization and routes

--- app/app.js
+++ app/app.js
@@ -1,14 +1,13 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static('app/public'));
+app.use(express.static('./app/public'));
+app.use(require('./routes/index'))
 
 app.set('view engine', 'ejs');
 app.set('views','app/views');
 
-app.get('/', function(request, response){
-  response.render('index');
-});
+
 
 app.listen(3030,function(){
   console.log('listening on port 3030');
